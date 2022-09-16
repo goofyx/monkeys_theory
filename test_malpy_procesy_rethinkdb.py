306464fc9366631bec84bc43632fbacb77ad8c3c
goofyx <k.faryna@goofyx.pl>
2022-09-16 12:11:03 +0200

    #3 więcej danych do bazy danych

--- test_malpy_procesy_rethinkdb.py
+++ test_malpy_procesy_rethinkdb.py
@@ -1,7 +1,3 @@
-#''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
-#''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
-#''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
-
 from concurrent.futures import process
 import string
 import random
@@ -11,6 +7,7 @@ import multiprocessing as mp
 import os
 import sys
 import getopt
+import socket
 
 from rethinkdb import RethinkDB 
 from rethinkdb.errors import RqlRuntimeError
@@ -24,16 +21,18 @@ def generuj_slowo(dlugosc_slowa):
 def timestamp_na_data(acctual_time):
     return datetime.datetime.fromtimestamp(acctual_time)
 
-def wynik_do_bazy(slowo):
+def wynik_do_bazy(slowo_zadane, slowo_wylosowane, nr_losowania):
     monkeys_database = RethinkDB()
-    monkeys_connection = monkeys_database.connect('192.168.200.181', 28015)
+    #monkeys_connection = monkeys_database.connect('192.168.200.181', 28015)
+    monkeys_connection = monkeys_database.connect('192.168.191.181', 28015)
     try:
-        monkeys_database.db("monkeys").table('wyniki').insert({ 'slowo_wylosowane': slowo }).run(monkeys_connection)
-
-#r.table("user").insert({
-#    "name": "Michel",
-#    "age": 26
-#}).run()
+        monkeys_database.db("monkeys").table('wyniki').insert({ 
+            "data_wylosowania": monkeys_database.epoch_time( time.time() ).to_iso8601(),
+            "nazwa_hosta": socket.gethostname(),
+            "slowo_szukane": slowo_zadane,
+            "slowo_wylosowane": slowo_wylosowane, 
+            "ilosc_losowan": nr_losowania
+        }).run(monkeys_connection)
 
     except RqlRuntimeError as err:
         print(err.message)
@@ -45,15 +44,16 @@ def wynik_do_bazy(slowo):
 def malpa_proces(nazwa, ilosc_losowan):    
     ilosc_trafien = 0
     czas_trafienia = 0
-    zapis_do_bazy = 1        
+    zapis_do_bazy = 1
+    ktore_losowanie = 0        
 
     czas_staru = time.time()
     print( nazwa, ': zaczął: ', timestamp_na_data(czas_staru) )
     
     for i in range( 1, ilosc_losowan + 1 ):
         slowo_losowanie = generuj_slowo( len( slowo_klucz ) )
-
-        #print(nazwa, ' iteracja: ', i, ' wylosowano: ', slowo_losowanie, ' lower: ', slowo_losowanie.lower())
+        ktore_losowanie += 1
+        
         if slowo_losowanie.lower() == slowo_klucz.lower():
         #if len(slowo_losowanie) > 1:
             ilosc_trafien += 1
@@ -66,7 +66,9 @@ def malpa_proces(nazwa, ilosc_losowan):
             
             #zapis do bazy
             if zapis_do_bazy == 1 :
-                wynik_do_bazy(slowo_losowanie)      
+                wynik_do_bazy(slowo_klucz, slowo_losowanie, ktore_losowanie)
+            
+            ktore_losowanie = 0     
 
         time.sleep(0)
 
@@ -80,9 +82,9 @@ def malpa_proces(nazwa, ilosc_losowan):
 
 
 
-slowo_klucz = 'krzys'
+slowo_klucz = 'monkey'
 ilosc_malp = 8
-ilosc_losowan = 9999999999 #12000000
+ilosc_losowan = 10000000000000
 #ilosc_trafien = 0
 malpy = []
 
